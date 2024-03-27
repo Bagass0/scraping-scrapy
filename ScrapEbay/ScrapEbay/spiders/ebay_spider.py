6dd3d1da89eeab3dab87a361df17d148a74a6cab
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2024-03-27 17:07:50 +0100

    updates tqdm + show sql request

--- ScrapEbay/ScrapEbay/spiders/ebay_spider.py
+++ ScrapEbay/ScrapEbay/spiders/ebay_spider.py
@@ -2,6 +2,7 @@ import scrapy
 import sqlite3
 import time
 import random
+from tqdm import tqdm
 
 class EbaySpiderSpider(scrapy.Spider):
     name = 'ebay_spider'
@@ -16,23 +17,22 @@ class EbaySpiderSpider(scrapy.Spider):
         self.conn.commit()
 
     def close(self, reason):
+        self.cursor.close()
         self.conn.close()
 
     def parse(self, response):
         products = response.css('.dne-itemtile')
-        for product in products:
+        num_products = len(products)
+        for i, product in enumerate(tqdm(products, total=num_products, desc="Scraping")):
             image = product.css('.dne-itemtile-imagewrapper a .slashui-image-cntr img::attr(src)').get()
             name = product.css('.dne-itemtile-detail .ebayui-ellipsis-2::text, .dne-itemtile-detail .ebayui-ellipsis-3::text').get()
             price = product.css('.dne-itemtile-detail .first::text').get()
             old_price = product.css('.dne-itemtile-detail .itemtile-price-strikethrough::text').get()
             discount = product.css('.dne-itemtile-detail .itemtile-price-bold::text').get()
-            yield {
-                'image': image,
-                'name': name,
-                'price': price,
-                'old price': old_price,
-                'discount': discount,
-            }
             self.cursor.execute("INSERT INTO products VALUES (?, ?, ?, ?, ?)", (image, name, price, old_price, discount))
             self.conn.commit()
             time.sleep(random.uniform(1, 2))
+        self.cursor.execute("SELECT * FROM products")
+        rows = self.cursor.fetchall()
+        for row in rows:
+            print(row)
