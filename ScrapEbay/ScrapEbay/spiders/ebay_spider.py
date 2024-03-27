ae9666fb2aab97da8a34e30bc52ce018cdcade03
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2024-03-27 13:16:37 +0100

    update

--- ScrapEbay/ScrapEbay/spiders/ebay_spider.py
+++ ScrapEbay/ScrapEbay/spiders/ebay_spider.py
@@ -8,13 +8,13 @@ class EbaySpiderSpider(scrapy.Spider):
     start_urls = ['https://www.ebay.com/deals']
 
     def parse(self, response):
-        products = response.css('.dne-itemtile, .dne-itemtile-detail')
+        products = response.css('.dne-itemtile')
         for product in products:
             image = product.css('.dne-itemtile-imagewrapper a .slashui-image-cntr img::attr(src)').get()
-            name = product.css('.ebayui-ellipsis-2::text').get()
-            price = product.css('.first::text').get()
-            oldprice = product.css('.itemtile-price-strikethrough::text').get()
-            discount = product.css('.itemtile-price-bold::text').get()
+            name = product.css('.dne-itemtile-detail .ebayui-ellipsis-2::text, .dne-itemtile-detail .ebayui-ellipsis-3').get()
+            price = product.css('.dne-itemtile-detail .first::text').get()
+            oldprice = product.css('.dne-itemtile-detail .itemtile-price-strikethrough::text').get()
+            discount = product.css('.dne-itemtile-detail .itemtile-price-bold::text').get()
             yield {
                 'image': image,
                 'name': name,
@@ -22,4 +22,4 @@ class EbaySpiderSpider(scrapy.Spider):
                 'old price': oldprice,
                 'discount': discount,
             }
-        time.sleep(random.uniform(1, 5))
+            time.sleep(random.uniform(1, 2))
