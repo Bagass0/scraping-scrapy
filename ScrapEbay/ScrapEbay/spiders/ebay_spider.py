e0fd8dc7a302c1964d3f6f9878b39e248341dbae
Bagass0 <80489889+Bagass0@users.noreply.github.com>
2024-03-27 15:51:11 +0100

    bdd sqlite3 + dockerfile

--- ScrapEbay/ScrapEbay/spiders/ebay_spider.py
+++ ScrapEbay/ScrapEbay/spiders/ebay_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import sqlite3
 import time
 import random
 
@@ -7,19 +8,31 @@ class EbaySpiderSpider(scrapy.Spider):
     allowed_domains = ['ebay.com']
     start_urls = ['https://www.ebay.com/deals']
 
+    def __init__(self):
+        self.conn = sqlite3.connect('ebay.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS products
+                            (image TEXT, name TEXT, price TEXT, old_price TEXT, discount TEXT)''')
+        self.conn.commit()
+
+    def close(self, reason):
+        self.conn.close()
+
     def parse(self, response):
         products = response.css('.dne-itemtile')
         for product in products:
             image = product.css('.dne-itemtile-imagewrapper a .slashui-image-cntr img::attr(src)').get()
-            name = product.css('.dne-itemtile-detail .ebayui-ellipsis-2::text, .dne-itemtile-detail .ebayui-ellipsis-3').get()
+            name = product.css('.dne-itemtile-detail .ebayui-ellipsis-2::text, .dne-itemtile-detail .ebayui-ellipsis-3::text').get()
             price = product.css('.dne-itemtile-detail .first::text').get()
-            oldprice = product.css('.dne-itemtile-detail .itemtile-price-strikethrough::text').get()
+            old_price = product.css('.dne-itemtile-detail .itemtile-price-strikethrough::text').get()
             discount = product.css('.dne-itemtile-detail .itemtile-price-bold::text').get()
             yield {
                 'image': image,
                 'name': name,
                 'price': price,
-                'old price': oldprice,
+                'old price': old_price,
                 'discount': discount,
             }
+            self.cursor.execute("INSERT INTO products VALUES (?, ?, ?, ?, ?)", (image, name, price, old_price, discount))
+            self.conn.commit()
             time.sleep(random.uniform(1, 2))
